29b8ce237f4259dd479fe79a562fd23d9cc03d4a
William Gotterer <william.gotterer@gmail.com>
2021-09-20 16:45:44 -0400

    commit tree 02c2e54336583c2ee4c538bf7a40e3cc9465c5ae

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet<=400){
+    return "This one is on me!"
+} else if(feet>2500){
+  return "No can do."
+} else {
+  return "I will gladly take your thirty bucks."
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ if(city==="NYC"){
+   return "Ok, sounds good."
+ }else{
+   return "No go."
+ }
+} 
+
+
+
+function switchOnCharmFromTip(tip){
+  if(tip==="generous"){
+    return "Thank you so much."
+  }else if(tip==="not as generous"){
+    return "Thank you."
+  }else{
+    return "Bye."
+  }
 }
